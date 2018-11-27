94cda963d57123b410f0578acacb22195907b596
raluca.josu <raluca.josu@gmail.com>
2018-11-27 18:06:54 +0200

    FirstCommit

---
+++ GuessTheNumber/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GuessTheNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rng = new Random();
+            int r = rng.Next(0, 100);
+
+            string input;
+            Console.WriteLine("Enter a number:");
+            input = Console.ReadLine();
+            int nr = int.Parse(input);
+            if (nr == r)
+            {
+                Console.WriteLine("You guessed the numebr!");
+            }
+            else {
+                while (nr != r)
+                {
+                    if (nr < r)
+                    {
+                        Console.WriteLine("The number is too small, please enter another one:");
+                        input = Console.ReadLine();
+                        nr = int.Parse(input);
+                    }
+                    else
+                    {
+                        Console.WriteLine("The number is too big, please enter another one:");
+                        input = Console.ReadLine();
+                        nr = int.Parse(input);
+                    }
+                }
+            }
+            }
+            
+        }
+    }
+
